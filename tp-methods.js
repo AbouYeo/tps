5b869f4e6c5abcd1cc63d09dba79f9c015dcec25
AbouYeo <kagnekoyeo@gmail.com>
2025-07-27 18:49:17 +0200

    Ajout du nom utilisateur

--- tp-methods.js
+++ tp-methods.js
@@ -1,27 +1,46 @@
 function calculer() {
-    const input1 = document.getElementById("firstNumber");
-    const input2 = document.getElementById("secondNumber");
+    const inputName = document.getElementById("name");
+    const inputFirstNumber = document.getElementById("firstNumber");
+    const inputSecondNumber = document.getElementById("secondNumber");
     const operation = document.getElementById("operation");
-    const res = document.getElementById("result");
+    const icon = document.getElementById("icon");
+    const res = document.getElementById("resultText");
 
-    let firstNumber = input1.value;
-    let secondNumber = input2.value;
+    let name = inputName.value.trim().split(" ");
+    let firstNumber = inputFirstNumber.value;
+    let secondNumber = inputSecondNumber.value;
     let op = operation.value;
     let result = 0;
+    let initiales = "";
+    let signeOperation = "";
 
     if (op == "addition") {
-        result = 1 * firstNumber + 1 * secondNumber;
+        result = Number(firstNumber) + Number(secondNumber);
+        signeOperation = "+";
     } else if (op == "substraction") {
-        result = 1 * firstNumber - 1 * secondNumber;
+        result = Number(firstNumber) - Number(secondNumber);
+        signeOperation = "-";
     } else if (op == "multiplication") {
-        result = 1 * firstNumber * 1 * secondNumber;
+        result = Number(firstNumber) * Number(secondNumber);
+        signeOperation = "*";
     } else if (op == "division") {
-        result = (1 * firstNumber) / (1 * secondNumber);
+        result = Number(firstNumber) / Number(secondNumber);
+        signeOperation = "/";
+    } else if (op == "modulo") {
+        result = Number(firstNumber) % Number(secondNumber);
+        signeOperation = "%";
     } else {
-        result = (1 * firstNumber) % (1 * secondNumber);
+        res.innerText = "Veuillez choisir une opération sil vous plait!";
     }
 
     console.log("Resultat: ", result);
+    console.log(name);
+    name = name.forEach((element) => {
+        element.trim();
+    });
+    console.log(name);
 
-    res.innerText = result;
+    icon.innerText = initiales;
+
+    res.innerText = `${name} ${firstNumber} ${signeOperation} ${secondNumber} =  ${result}`;
 }
