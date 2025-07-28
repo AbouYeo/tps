1bcd687485a0b548c1d9bda4bd03815001b8accc
AbouYeo <kagnekoyeo@gmail.com>
2025-07-28 10:34:28 +0200

    Récuperation des données avec fetch()

---
+++ tp7-users.js
@@ -0,0 +1,10 @@
+function fetchUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            return data;
+        });
+}
