c628b5d3876e7e2e8da050c76c066d6b5e242bfb
AbouYeo <kagnekoyeo@gmail.com>
2025-07-29 09:43:42 +0200

    Affichage des données dans le tableau

--- tp7-users.js
+++ tp7-users.js
@@ -2,14 +2,22 @@ function handlResponse(response) {
     return response.json();
 }
 
+let tableLines = "";
+const tBodyUsers = document.getElementById("tbody-users");
 function handleData(data) {
     console.log(data);
 
     let users = data;
     for (index = 0; index < users.length; index++) {
         let user = users[index];
-        console.log(user);
+        let line = `<tr>
+                        <td>${user.name}</td>
+                        <td>${user.phone}</td>
+                        <td>${user.email}</td>
+                    </tr>`;
+        tableLines += line;
     }
+    tBodyUsers.innerHTML = tableLines;
 }
 
 function fetchUsers() {
